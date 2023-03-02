76a33a57ce6bbd37188e78f90c4e0a04d14cc001
arnonaoc <arnon@asia1click.com>
2023-03-02 09:34:37 +0700

    asdasd

--- src/App.jsx
+++ src/App.jsx
@@ -21,7 +21,7 @@ function App() {
           count is {count}
         </button>
         <p>
-          Edit <code>src/App.jsx</code> and save to teหasasdasddกsdsดasdกหst HMR
+          Edit <code>src/App.jsx</code> and save asdsto teหasasdasddกsdsดasdกหst HMR
         </p>
       </div>
       <p className="read-the-docs">
